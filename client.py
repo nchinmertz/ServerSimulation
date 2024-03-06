604bb054981b10ca80b248613b12963864a21e2c
nchinmertz <48361293+nchinmertz@users.noreply.github.com>
2024-03-06 05:48:20 -0500

    file upload

--- client.py
+++ client.py
@@ -1,17 +1,24 @@
 import socket
+import random
 import time
 
 
 class Client:
-    def __init__(self, host, port, website):
-        self.host = host
-        self.port = 65431 # random
-        self.welcome_port = port
-        self.website_name = website
-        self.request = ""
+    def __init__(self, host_ip, server_welcome_port):
+        self.host_ip = host_ip
+        self.server_welcome_port = server_welcome_port
+        self.ports_used = [server_welcome_port]
+        self.sockets_used = []
+
+    def get_new_port(self):
+        while True:
+            num = random.randint(1024, 65535)
+            if num not in self.ports_used:
+                self.ports_used.append(num)
+                return num
 
     @staticmethod
-    def make_packet(packet_type, source, dest, data=""):
+    def encode(packet_type, source, dest, data=""):
         line2str = ""
         if packet_type == "ack":
             line2str = "1000"
@@ -19,7 +26,7 @@ class Client:
             line2str = "0100"
         elif packet_type == "fin":
             line2str = "0010"
-        elif packet_type == "ping":
+        elif packet_type == "urg":
             line2str = "0001"
         elif packet_type == "syn/ack":
             line2str = "1001"
@@ -29,7 +36,7 @@ class Client:
         return header.encode()
 
     @staticmethod
-    def decode_message(message):
+    def decode(message):
         message = message.decode()
         message_type = ""
         if int(message[35]) == 1 and int(message[32]) == 1:
@@ -39,50 +46,63 @@ class Client:
         elif int(message[34]) == 1:
             message_type = "fin"
         elif int(message[35]) == 1:
-            message_type = "pong"
+            message_type = "urg"
         elif int(message[32]) == 1:
             message_type = "ack"
         return {"source": int(message[:16], 2), "dest": int(message[16:32], 2), "message type": message_type,
                 "data": message[36:]}
 
-    def tcp_connect(self, ip_address):
-        message = f"GET / HTTP/1.1\r\nHost:www.{self.website_name}\r\n\r\n".encode()
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(10000)
+    def new_socket(self, s, port, server_port, domain):
         try:
-            s.connect((ip_address, 80))
-            s.sendto(message, (ip_address, 80))
-            response = s.recvfrom(512)
-            response = repr(response)
-        except socket.timeout:
-            return 0, 0
-        with open(f"{self.website_name[:-4]}.html", "w") as file:
-            file.write(response)
+            s.connect((self.host_ip, server_port))
+            time.sleep(3)
+            while True:
+                message = self.encode(None, port, server_port, "ping")
+                s.sendto(message, (self.host_ip, server_port))
+                message = s.recv(1024)
+                message = self.decode(message)
+                if message["data"] == "pong":
+                    print(f"Client at port {port} received pong from server at {server_port}")
+        except KeyboardInterrupt:
+            s.close()
+
+    def welcome(self):
+        welcome_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        welcome_port = welcome_socket.getsockname()[1]
+        welcome_socket.bind((self.host_ip, welcome_port))
+        self.ports_used.append(welcome_port)
+        self.sockets_used.append(welcome_socket)
+        try:
+            # domain = input("Please input a website name: ")
+            welcome_socket.connect((self.host_ip, self.server_welcome_port))
+            print("Sending SYN")
+            message = self.encode("syn", welcome_port, self.server_welcome_port)
+            welcome_socket.sendto(message, (self.host_ip, self.server_welcome_port))
+            data = welcome_socket.recv(1024)
+            data = self.decode(data)
+            if data["message type"] == "syn/ack":
+                new_server_port = int(data["data"])
+                self.ports_used.append(new_server_port)
+                print(f"Received SYN/ACK, new server port at {new_server_port}")
+                new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                new_port = self.get_new_port()
+                new_socket.bind((self.host_ip, new_port))
+                self.sockets_used.append(new_socket)
+                print(f"New Socket Created at Port {new_port}, sending ACK")
+                message = self.encode("ack", new_port, self.server_welcome_port, str(new_port))
+                welcome_socket.sendto(message, (self.host_ip, self.server_welcome_port))
+                self.new_socket(new_socket, new_port, new_server_port, domain="tmz.com")
+        except KeyboardInterrupt:
+            welcome_socket.close()
 
     def run(self):
-        message = self.make_packet("ping", self.port, self.welcome_port, self.website_name)
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        time.sleep(3)
-        s.sendto(message, (self.host, self.welcome_port))
-        print("Message sent")
-        s.settimeout(10000)
-        try:
-            message, server_address = s.recvfrom(1024)
-            message = self.decode_message(message)
-            if message["message type"] == "pong":
-                ip_address = message["data"]
-                print("Make TCP connection")
-                # self.tcp_connect(ip_address)
-        except socket.timeout:
-            s.close()
+        self.welcome()
 
 
 def main():
-    host = "192.168.0.136"
-    welcome_port = 65432
-    website = "tmz.com"
-    client = Client(host, welcome_port, website)
+    ip = "192.168.0.136"
+    server_welcome_port = 65432
+    client = Client(ip, server_welcome_port)
     client.run()
-
 
 main()
--- server.py
+++ server.py
@@ -1,14 +1,22 @@
 import socket
-
+import random
 
 class Server:
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
-        self.socket = None
+    def __init__(self, host_ip, welcome_port):
+        self.host_ip = host_ip
+        self.welcome_port = welcome_port
+        self.ports_used = [welcome_port]
+        self.sockets_used = []
+
+    def get_new_port(self):
+        while True:
+            num = random.randint(1024, 65535)
+            if num not in self.ports_used:
+                self.ports_used.append(num)
+                return num
 
     @staticmethod
-    def make_packet(packet_type, source, dest, data=""):
+    def encode(packet_type, source, dest, data=""):
         line2str = ""
         if packet_type == "ack":
             line2str = "1000"
@@ -16,7 +24,7 @@ class Server:
             line2str = "0100"
         elif packet_type == "fin":
             line2str = "0010"
-        elif packet_type == "pong":
+        elif packet_type == "urg":
             line2str = "0001"
         elif packet_type == "syn/ack":
             line2str = "1001"
@@ -26,45 +34,71 @@ class Server:
         return header.encode()
 
     @staticmethod
-    def decode_message(message):
+    def decode(message):
         message = message.decode()
         message_type = ""
-        if int(message[32]) == 1:
-            message_type = "ack"
+        if int(message[35]) == 1 and int(message[32]) == 1:
+            message_type = "syn/ack"
         elif int(message[33]) == 1:
             message_type = "syn"
         elif int(message[34]) == 1:
             message_type = "fin"
         elif int(message[35]) == 1:
-            message_type = "ping"
-        elif int(message[35]) == 1 and int(message[32]) == 1:
-            message_type = "syn/ack"
-        return {"source": int(message[:16], 2), "dest": int(message[16:32], 2),
-                "message type": message_type, "data": message[36:]}
+            message_type = "urg"
+        elif int(message[32]) == 1:
+            message_type = "ack"
+        return {"source": int(message[:16], 2), "dest": int(message[16:32], 2), "message type": message_type,
+                "data": message[36:]}
+
+    def new_socket(self, s, port, client_port):
+        try:
+            s.listen(1)
+            s, addr = s.accept()
+            print(f"Port {port} is listening")
+            while True:
+                message = s.recv(1024)
+                message = self.decode(message)
+                if message["data"] == "ping":
+                    print(f"Server at port {port} received ping from client at {client_port}")
+                    message = self.encode(None, port, client_port, "pong")
+                    s.sendto(message, (self.host_ip, client_port))
+        except KeyboardInterrupt:
+            s.close()
+
+    def welcome(self):
+        welcome_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        welcome_socket.bind((self.host_ip, self.welcome_port))
+        self.sockets_used.append(welcome_socket)
+        welcome_socket.listen(1)
+        welcome_socket, addr = welcome_socket.accept()
+        print("Listening")
+        try:
+            message = welcome_socket.recv(1024)
+            self.ports_used.append(addr[1])
+            message = self.decode(message)
+            if message["message type"] == "syn":
+                new_port = self.get_new_port()
+                new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                new_socket.bind((self.host_ip, new_port))
+                print(f"Received SYN, sending SYN/ACK, new port created at {new_port}")
+                message = self.encode("syn/ack", self.welcome_port, addr[1], str(new_port))
+                welcome_socket.sendto(message, addr)
+                message = welcome_socket.recv(1024)
+                message = self.decode(message)
+                if message["message type"] == "ack":
+                    new_client_port = int(message["data"])
+                    self.new_socket(new_socket, new_port, new_client_port)
+        except KeyboardInterrupt:
+            welcome_socket.close()
 
     def run(self):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.socket.bind((self.host, self.port))
-        self.socket.settimeout(10000)
-        print("listening")
-        try:
-            message, client_address = self.socket.recvfrom(1024)
-            print("message received")
-            message = self.decode_message(message)
-            if message["message type"] == "ping":
-                print("Traverse Tree")
-                # make and send packet
-
-            elif message["message type"] == "fin":
-                self.socket.close()
-        except KeyboardInterrupt:
-            self.socket.close()
+        self.welcome()
 
 
 def main():
-    host = "192.168.0.136"
-    port = 65432
-    server = Server(host, port)
+    ip = "192.168.0.136"
+    server_welcome_port = 65432
+    server = Server(ip, server_welcome_port)
     server.run()
 
 
